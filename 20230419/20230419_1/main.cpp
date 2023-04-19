6a5d58b2abdc42a88ad4a47dbb222cb38b70b185
hyeonseo1234 <125714813+hyeonseo1234@users.noreply.github.com>
2023-04-19 16:08:23 +0900

    Add files via upload

---
+++ 20230419/20230419_1/main.cpp
@@ -0,0 +1,31 @@
+// 숫자 추측 게임
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void)
+{
+	srand((unsigned)time(NULL));    // 난수 발생기 시드 설정
+
+	int answer = rand() % 100 + 1;  // 정답을 난수로 발생한다.
+	int guess;
+	int tries = 0;
+	// 반복 구조
+	do 
+	{
+		printf("정답을 추측해 보시어: ");
+		scanf("%d", &guess);
+		tries++;
+
+		if (guess > answer)
+			printf("HIGH \n");  // 사용자가 입력된 정수가 정답보다 높으면
+		if (guess < answer)
+			printf("LOW \n");   // 사용자가 입력된 정수가 정답보다 낮으면
+
+	} while (guess != answer);
+
+	printf("축하합니다. 시도횟수=%d \n", tries);
+	return 0;
+}
