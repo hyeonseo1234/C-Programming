24458783db5f94e3fbbdb26b0a486e65d3ef701a
hyeonseo1234 <125714813+hyeonseo1234@users.noreply.github.com>
2023-04-23 22:23:55 +0900

    Add files via upload

---
+++ 20230423/20230423_3/main.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+	int i, n, sum;
+	
+	printf("정수를 입력하시오: ");
+	scanf("%d", &n);
+
+	i = 0;
+	sum = 0;
+
+	while (i <= n)
+	{
+		sum += i;
+		i = i + 2;  // i += 2;로 하여도 된다.
+	}
+
+	printf("1부터 %d까지의 합은 %d입니다\n", n, sum);
+	return 0;
+
+}
