67a14dde8d01b735f41229ce020596b429487612
hyeonseo1234 <125714813+hyeonseo1234@users.noreply.github.com>
2023-04-23 22:53:45 +0900

    Add files via upload

---
+++ 20230423/20230423_24/main.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(void)
+{
+	int x, y;
+
+	for (y = 1; y < 10000; y++)
+	{
+		for (x = 1; x < 10; x++)
+		{
+			if ( _kbhit() )
+				goto OUT;
+			printf("*");
+
+		}
+		printf("\n");
+	}
+OUT:
+	
+	return 0;
+}
